e74b271e51107019fc221fa78121d43d7f231bf6
Yuliana <ya.aldrich@yahoo.com>
2021-03-10 00:38:54 +0000

    Final commit

--- sales_report.py
+++ sales_report.py
@@ -1,25 +1,36 @@
 """Generate sales report showing total melons each salesperson sold."""
 
-
+#we need to store a list when looking for salespeople
 salespeople = []
+#the melons that have been sold 
 melons_sold = []
 
+#opening the file
 f = open('sales-report.txt')
+
+#iterating line to find out the f value
 for line in f:
-    line = line.rstrip()
-    entries = line.split('|')
+    line = line.rstrip() #remove space on the right
+    entries = line.split('|') #separate each item in the line
 
-    salesperson = entries[0]
-    melons = int(entries[2])
+    salesperson = entries[0] # creating a new item to store the first value
+    melons = int(entries[2]) # creating melons to restore the second value of line
+                             # its a number so it will be a integrer
 
-    if salesperson in salespeople:
-        position = salespeople.index(salesperson)
+    if salesperson in salespeople: #if the salesperson is in salespeople, store the value in:
+        position = salespeople.index(salesperson) #from the salespeople we are taking the salesperson
+                                   #store it in position
 
-        melons_sold[position] += melons
-    else:
-        salespeople.append(salesperson)
-        melons_sold.append(melons)
+#how do we know this position index has to be in the position?
+
+        melons_sold[position] += melons #looking for the position of the person who has sold the 
+                                        #the melons 
+
+    else: #if we dont find what we are looking for
+        salespeople.append(salesperson) #if salesperson doesnt exist, add it to the salespeople
+        melons_sold.append(melons) #add melons to melons sold?
 
 
-for i in range(len(salespeople)):
+for i in range(len(salespeople)): #please explain again
+    
     print(f'{salespeople[i]} sold {melons_sold[i]} melons')
